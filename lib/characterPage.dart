139e1eb8e5f2bfeed5e49adc163ebe52634a15ef
Nelfunction <oat641@gmail.com>
2020-11-06 21:19:30 +0900

    lowercase

---
+++ lib/characterPage.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class CharacterPage extends StatefulWidget {
+  @override
+  _CharacterPageState createState() => _CharacterPageState();
+}
+
+class _CharacterPageState extends State<CharacterPage> {
+  // 개같이 대충 코딩한 부분임.
+  // input이 입력받은 데이터임.
+  List<List<String>> input = [
+    ["", "BlackWhite", "Black", "Flame"],
+    ["Q", "Q", "Q", "Q"],
+    ["Q", "Q", "Q", "Q"],
+    ["Q", "Q", "Q", "Q"],
+    ["Q", "Q", "Q", "Q"],
+    ["Q", "Q", "Q", "Q"],
+    ["Q", "Q", "Q", "Q"]
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+          child: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 70),
+            Text(
+              'Characters',
+              style: TextStyle(fontSize: 36, color: Colors.white),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  padding: EdgeInsets.all(10),
+                  shrinkWrap: true,
+                  itemCount: input.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CharaterRow(
+                      char1: input[index][0],
+                      char2: input[index][1],
+                      char3: input[index][2],
+                      char4: input[index][3],
+                    );
+                  }),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class CharaterRow extends StatefulWidget {
+  final String char1, char2, char3, char4;
+
+  const CharaterRow(
+      {this.char1 = "", this.char2 = "", this.char3 = "", this.char4 = ""});
+
+  _CharaterRowState createState() => _CharaterRowState();
+}
+
+class _CharaterRowState extends State<CharaterRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CharacterContainer(nameChar: widget.char1),
+          CharacterContainer(nameChar: widget.char2),
+          CharacterContainer(nameChar: widget.char3),
+          CharacterContainer(nameChar: widget.char4),
+        ],
+      ),
+    );
+  }
+}
+
+class CharacterContainer extends StatelessWidget {
+  final String nameChar;
+  const CharacterContainer({this.nameChar = ""});
+
+  @override
+  Widget build(BuildContext context) {
+    String nameText;
+    if (nameChar == "Q") {
+      nameText = "???";
+    } else {
+      nameText = nameChar + ' Cat';
+    }
+
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: Colors.white, width: 4),
+            color: Color.fromARGB(100, 255, 255, 255),
+          ),
+          margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+          padding: EdgeInsets.all(10),
+          child: SizedBox(
+            width: 40,
+            height: 40,
+            child: Image.asset("assets/images/kittenIcon" + nameChar + ".png"),
+          ),
+        ),
+        Text(
+          nameText,
+          style: TextStyle(fontSize: 10, color: Colors.white),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'game_screen.dart';
+import 'global.dart';
+import 'login.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:walkerholic_sprite/pedometer.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+double catsize = 200;
+
+class _MyHomeState extends State<MyHome> {
+  TextStyle thumbnailStyle =
+      new TextStyle(fontSize: 20, fontWeight: FontWeight.w600, shadows: [
+    Shadow(
+        // bottomLeft
+        offset: Offset(-1.5, -1.5),
+        color: Colors.black),
+    Shadow(
+        // bottomRight
+        offset: Offset(1.5, -1.5),
+        color: Colors.black),
+    Shadow(
+        // topRight
+        offset: Offset(1.5, 1.5),
+        color: Colors.black),
+    Shadow(
+        // topLeft
+        offset: Offset(-1.5, 1.5),
+        color: Colors.black),
+  ]);
+
+  //MyGame game = MyGame();
+  TextEditingController controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white,
+          buttonColor: Colors.black),
+      home: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: gamecards.length,
+          itemBuilder: (context, index) {
+            return temp(gamecards[index]);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget temp(gamecard Gamecard) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 60, 30, 40),
+      child: InkWell(
+          onTap: () {
+            print("tapped!");
+            Navigator.of(context).push(createRoute(FullGame(Gamecard)));
+          },
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: new AssetImage('assets/images/pixel_background' +
+                          Gamecard.character.toString() +
+                          '.jpg'),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: Colors.transparent, width: 150),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(0.0, 2.0), //(x,y)
+                      blurRadius: 6.0,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      Gamecard.name,
+                      style: thumbnailStyle,
+                    ),
+                    Text(
+                      (Gamecard.steps).toString() + " Steps",
+                      style: thumbnailStyle,
+                    )
+                  ],
+                ),
+                right: 20,
+                bottom: 20,
+              )
+            ],
+          )),
+    );
+  }
+}
+
+class gamecard {
+  String name;
+  int steps;
+  int character;
+  gamecard(String name, int steps, int character) {
+    this.name = name;
+    this.steps = steps;
+    this.character = character;
+  }
+}
+
+String getdate(DateTime date) {
+  var date_YMD = "${date.year}-${date.month}-${date.day}";
+  return date_YMD;
+}
+
+//cloud firestore 친구 목록에서부터 친구들의 오늘의 steps을 gamecards에 넣는 함수
+Future<void> loadfrienddata() async {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  int gamecardstep = 0;
+  int character = 1;
+  await FirebaseFirestore.instance //자신의 gamecard 를 생성
+      .collection(userid)
+      .doc(getdate(DateTime.now()))
+      .get()
+      .then((DocumentSnapshot documentSnapshot) {
+    if (documentSnapshot.exists) {
+      gamecardstep = documentSnapshot.get('steps');
+    } else {
+      gamecardstep = 0;
+    }
+  });
+  await FirebaseFirestore.instance //자신의 gamecard 를 생성
+      .collection(userid)
+      .doc('character')
+      .get()
+      .then((DocumentSnapshot documentSnapshot) {
+    if (documentSnapshot.exists) {
+      character = documentSnapshot.get('character');
+    } else {
+      character = 1;
+    }
+  });
+
+  gamecards.add(gamecard(userid, gamecardstep, character)); //gamecards에 추가
+
+  int friendnum = 0;
+
+  await FirebaseFirestore.instance //친구의 숫자를 받음
+      .collection(userid)
+      .doc('friend_list')
+      .get()
+      .then((DocumentSnapshot documentSnapshot) {
+    if (documentSnapshot.exists) {
+      friendnum = documentSnapshot.get('num');
+    } else {
+      datastep = steps;
+    }
+  });
+  for (int i = 1; i <= friendnum; i++) //친구의 숫자만큼 gamecards에 추가
+  {
+    String temp = 'name' + i.toString();
+    String friendname = 'temp';
+    await FirebaseFirestore.instance
+        .collection(userid)
+        .doc('friend_list')
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        friendname = documentSnapshot.get(temp);
+      } else {
+        friendname = 'null';
+      }
+    });
+    await FirebaseFirestore.instance
+        .collection(friendname)
+        .doc(getdate(DateTime.now()))
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        gamecardstep = documentSnapshot.get('steps');
+      } else {
+        gamecardstep = 99;
+      }
+    });
+    await FirebaseFirestore.instance //자신의 gamecard 를 생성
+        .collection(friendname)
+        .doc('character')
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        character = documentSnapshot.get('character');
+      } else {
+        character = 1;
+      }
+    });
+
+    gamecards.add(gamecard(friendname, gamecardstep, character));
+  }
+}
+
+Future<int> loadAsyncData() async {
+  return datastep;
+}
